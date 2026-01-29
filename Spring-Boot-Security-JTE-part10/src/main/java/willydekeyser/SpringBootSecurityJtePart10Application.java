548073b0e06c9d4476cf42a9e5bb832d8f2e1e04
Willy De Keyser <spring.boot.programming@gmail.com>
2026-01-29 17:20:59 +0100

    Setup

---
+++ Spring-Boot-Security-JTE-part10/src/main/java/willydekeyser/SpringBootSecurityJtePart10Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJtePart10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJtePart10Application.class, args);
+	}
+
+}
