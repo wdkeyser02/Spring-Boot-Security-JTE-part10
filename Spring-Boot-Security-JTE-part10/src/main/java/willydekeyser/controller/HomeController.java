94cda1b12d02aa65e0ed37bb2244f460c33ae3bd
Willy De Keyser <spring.boot.programming@gmail.com>
2026-01-30 14:58:15 +0100

    Update Html

--- Spring-Boot-Security-JTE-part10/src/main/java/willydekeyser/controller/HomeController.java
+++ Spring-Boot-Security-JTE-part10/src/main/java/willydekeyser/controller/HomeController.java
@@ -3,6 +3,7 @@ package willydekeyser.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import willydekeyser.config.CustomUser;
@@ -10,23 +11,42 @@ import willydekeyser.config.CustomUser;
 @Controller
 public class HomeController {
 	
+	private String userName = "Anonymous";
+	
     @GetMapping("/")
-    public String home(Authentication authentication) {
+    public String home(Model model ,Authentication authentication) {
+    	if (authentication != null) {
+            userName = authentication.getName().toUpperCase();
+            model.addAttribute("roles", authentication.getAuthorities().toString());
+        } else {
+            userName = "Anonymous";
+            model.addAttribute("roles", "");
+        }
+    	model.addAttribute("userName", userName);
+    	
         return "index";
     }
 
     @GetMapping("/public")
-    public String public_page(Authentication authentication) {
+    public String public_page(Model model ,Authentication authentication) {
+    	if (authentication != null) {
+            userName = authentication.getName().toUpperCase();
+        } else {
+            userName = "Anonymous";
+        }
+    	model.addAttribute("userName", userName);
         return "public";
     }
 
     @GetMapping("/user")
-    public String private_page_user(@AuthenticationPrincipal CustomUser user) {
+    public String private_page_user(Model model ,@AuthenticationPrincipal CustomUser user) {
+    	model.addAttribute("user", user);
         return "user";
     }
 
     @GetMapping("/admin")
-    public String private_page_admin(@AuthenticationPrincipal CustomUser user) {
+    public String private_page_admin(Model model ,@AuthenticationPrincipal CustomUser user) {
+    	model.addAttribute("user", user);
         return "admin";
     }
 }
